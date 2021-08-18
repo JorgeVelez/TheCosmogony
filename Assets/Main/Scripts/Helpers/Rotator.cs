dd50208030c6c6bff78fe0ec8b81fca916d5a467
jorgevelez <jorgeluisvelez@gmail.com>
2021-08-18 11:39:52 -0700

    update modelos, video anims

--- Assets/Main/Scripts/Helpers/Rotator.cs
+++ Assets/Main/Scripts/Helpers/Rotator.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class Rotator : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool XAxis=false;
+    public float XSpeed=1;
+    public bool YAxis=false;
+    public float YSpeed=1;
+    public bool ZAxis=false;
+    public float ZSpeed=1;
+
+
+bool  GlobalStop=true;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0,.6f,0, Space.Self);
+        if(!GlobalStop)
+        transform.Rotate(XAxis?XSpeed:0, YAxis?YSpeed:0, ZAxis?ZSpeed:0, Space.Self);
 
     }
 }
