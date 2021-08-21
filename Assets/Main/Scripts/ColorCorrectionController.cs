6394bb692539343e15fb4d43b8bb674866d647f8
jorgevelez <jorgeluisvelez@gmail.com>
2021-08-21 08:49:35 -0700

    multiple color correction

--- Assets/Main/Scripts/ColorCorrectionController.cs
+++ Assets/Main/Scripts/ColorCorrectionController.cs
@@ -8,22 +8,36 @@ public class ColorCorrectionController : Singleton<ColorCorrectionController>
 {
     public GameObject sliderPrefab;
 
+    public Slider globalSlider;
+
     public List<Volume> volumes;
 
-
+    public List<GameObject> icons;
         public GameObject UI;
 
     void Start()
     {
+
+
         foreach (Volume item in volumes)
         {
             Slider sli=Instantiate(sliderPrefab, sliderPrefab.transform.parent).GetComponent<Slider>();
             sli.gameObject.SetActive(true);
+            sli.transform.Find("Label").GetComponent<Text>().text=item.gameObject.name;
+
             Volume tempVol=item;
+            item.weight=0;
+
             sli.onValueChanged.AddListener((val)=>{
+            sli.transform.Find("Value").GetComponent<Text>().text=val.ToString("F2");
                 tempVol.weight=val;
             });
         }
+
+                globalSlider.onValueChanged.AddListener((val)=>{
+                globalSlider.transform.Find("Label").GetComponent<Text>().text="Global";
+                globalSlider.transform.Find("Value").GetComponent<Text>().text=val.ToString("F2");
+            });
     }
 
     // Update is called once per frame
