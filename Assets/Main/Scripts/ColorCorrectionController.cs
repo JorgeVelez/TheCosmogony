e3571dfbbf5fded78c2f19a9880c4ec9c27e0d85
jorgevelez <jorgeluisvelez@gmail.com>
2021-08-22 11:23:32 -0700

    primer shoot at animacion

--- Assets/Main/Scripts/ColorCorrectionController.cs
+++ Assets/Main/Scripts/ColorCorrectionController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,77 +6,104 @@ using UnityEngine.Rendering;
 
 public class ColorCorrectionController : Singleton<ColorCorrectionController>
 {
+    private float elapsedTime = 0;
+    private float totalTime = 0;
+    private bool MonitorTime = false;
+
+    enum DayState {ComienzoDia, Dia, ComienzoNoche, Noche};
+
+    private DayState estado;
+    private int currentVol;
+    private int previousVol;
+    private int nextVol;
+
+    private float DuracionDia;
+    private float DuracionNoche;
+    private float DuracionTransicion;
+
+    public InputField iFieldDuracionDia;
+
+    public InputField iFieldDuracionNoche;
+
+    public InputField iFieldDuracionTransicion;
+
     public GameObject sliderPrefab;
-
     public Slider globalSlider;
-
-    public List<Volume> volumes;
-
-    private List<Slider> sliders=new List<Slider>();
-    
-    public List<GameObject> icons;
     public GameObject UI;
-
     public Button PLayBt;
     public Button PauseBt;
-     public InputField iFieldTimePerForecast;
 
-    private bool MonitorTime=false;
+    public List<Volume> volumes;
+    private List<Slider> sliders = new List<Slider>();
+    public List<GameObject> icons;
 
-    private float elapsedTime=0;
-
-    private float totalTime=0;
-    private float timeperForecast=0;
 
     void Start()
     {
-        timeperForecast=float.Parse(iFieldTimePerForecast.text);
+        DuracionDia = float.Parse(iFieldDuracionDia.text);
+
+        DuracionNoche = float.Parse(iFieldDuracionNoche.text);
+
+        DuracionTransicion = float.Parse(iFieldDuracionTransicion.text);
 
         for (int i = 0; i < volumes.Count; i++)
         {
-            Slider sli=Instantiate(sliderPrefab, sliderPrefab.transform.parent).GetComponent<Slider>();
+            Slider sli = Instantiate(sliderPrefab, sliderPrefab.transform.parent).GetComponent<Slider>();
             sli.gameObject.SetActive(true);
-            sli.transform.Find("Label").GetComponent<Text>().text=volumes[i].gameObject.name;
-sliders.Add(sli);
-            Volume tempVol=volumes[i];
-            volumes[i].weight=0;
+            sli.transform.Find("Label").GetComponent<Text>().text = volumes[i].gameObject.name;
+            sliders.Add(sli);
+            //Volume tempVol=volumes[i];
+            volumes[i].weight = 0;
 
-            sli.onValueChanged.AddListener((val)=>{
-            sli.transform.Find("Value").GetComponent<Text>().text=val.ToString("F2");
-                tempVol.weight=val;
+            sli.onValueChanged.AddListener((val) =>
+            {
+                sli.transform.Find("Value").GetComponent<Text>().text = val.ToString("F2");
+                //tempVol.weight=val;
             });
         }
 
-        totalTime=timeperForecast*volumes.Count;
+        totalTime = DuracionDia + DuracionNoche;
 
-        globalSlider.transform.Find("Label").GetComponent<Text>().text="Global";
-        globalSlider.maxValue=totalTime;
+        globalSlider.transform.Find("Label").GetComponent<Text>().text = "Global";
+        globalSlider.maxValue = totalTime;
 
-        globalSlider.onValueChanged.AddListener((val)=>{
-        globalSlider.transform.Find("Value").GetComponent<Text>().text=val.ToString("F2");
+        currentVol = UnityEngine.Random.Range(0, volumes.Count / 2);
+        GenerateNextWeather();
+        previousVol=-1;
+        estado=DayState.ComienzoDia;
 
-        int currentVol=Mathf.FloorToInt(val/timeperForecast);
-        //volumes[currentVol].weight=val-currentVol;
+        //////////////////////////////////////////////////////////////////////
+        globalSlider.onValueChanged.AddListener((hora) =>
+        {
+            TimeSpan time = TimeSpan.FromHours(hora);
+            globalSlider.transform.Find("Value").GetComponent<Text>().text =time.ToString(@"hh\:mm");
 
-        float currentValue=((val/timeperForecast)-currentVol);
-        sliders[currentVol].value=currentValue;
+            if(estado==DayState.ComienzoDia && hora<DuracionTransicion){
+                //hora/DuracionTransicion;
+            }
 
-        for (int j = 0; j < icons.Count; j++)
-            icons[j].SetActive(false);
-        icons[currentVol].SetActive(true);
+            /* float currentValue = ((val) - currentVol);
+            sliders[currentVol].value = currentValue;
 
-        if(currentVol>0){
-        sliders[currentVol-1].value=1-currentValue;
-        if(sliders[currentVol-1].value>sliders[currentVol].value){
-            icons[currentVol].SetActive(false);
-            icons[currentVol-1].SetActive(true);
+            for (int j = 0; j < icons.Count; j++)
+                icons[j].SetActive(false);
+            icons[currentVol].SetActive(true);
 
-        }
+            if (currentVol > 0)
+            {
+                sliders[currentVol - 1].value = 1 - currentValue;
+                if (sliders[currentVol - 1].value > sliders[currentVol].value)
+                {
+                    icons[currentVol].SetActive(false);
+                    icons[currentVol - 1].SetActive(true);
 
-        }
-        
+                }
+
+            } */
+
 
         });
+        ///////////////////////////////////////////////////////////
 
         PLayBt.onClick.AddListener(playAnimation);
         PauseBt.onClick.AddListener(pauseAnimation);
@@ -84,55 +111,66 @@ sliders.Add(sli);
         Hide();
     }
 
-    // Update is called once per frame
+    void GenerateNextWeather()
+    {
+        nextVol = UnityEngine.Random.Range(0, volumes.Count / 2);
+        while (nextVol == currentVol)
+        {
+            nextVol = UnityEngine.Random.Range(0, volumes.Count / 2);
+        }
+
+
+    }
     void Update()
     {
-        if(MonitorTime)
+        if (MonitorTime)
         {
-
             elapsedTime += Time.deltaTime;
 
-            globalSlider.value=elapsedTime;
+            globalSlider.value = elapsedTime;
 
-            if(elapsedTime>totalTime)
-            MonitorTime=false;
+            if (elapsedTime > totalTime){
+                MonitorTime = false;
+                estado=DayState.ComienzoDia;
+                elapsedTime=0;
+            }
         }
     }
 
-    void playAnimation()    
+    void playAnimation()
     {
-        MonitorTime=true;
+        MonitorTime = true;
         PLayBt.gameObject.SetActive(false);
         PauseBt.gameObject.SetActive(true);
 
     }
-     void pauseAnimation()    
+    void pauseAnimation()
     {
-        MonitorTime=false;
+        MonitorTime = false;
         PauseBt.gameObject.SetActive(false);
         PLayBt.gameObject.SetActive(true);
 
 
     }
 
-      internal void toggleVisibility()
-    { 
- 
+    internal void toggleVisibility()
+    {
+
         Debug.Log("<color=green>toggleVisibility " + UI.gameObject.activeSelf + "</color>");
- 
+
         if (!UI.gameObject.activeSelf)
             Show();
         else
             Hide();
     }
-  public void Show()
+    public void Show()
     {
         UI.gameObject.SetActive(true);
- 
-     }
+
+    }
     public void Hide()
     {
         UI.gameObject.SetActive(false);
- 
+
     }
 }
